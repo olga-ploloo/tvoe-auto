d467778727b87c97d1db37a1beba4f202ffe15ab
olga-ploloo <olga.kozintsava@gmail.com>
2020-09-21 19:16:48 +0300

    add admin

--- avto/admin.py
+++ avto/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from avto.models import Category, Model, ModelImage, Brend, Generation, Equipment
+
+class BrendAdmin(admin.ModelAdmin):
+    model = Brend
+    #fields = ('title', 'country')
+
+class ModelImageInline(admin.TabularInline):
+    model = ModelImage
+    fields = ('description', 'image', 'image_base64')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'get_subcategories')
+    filter_vertical = ('subcategories', )
+
+
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'generations')
+
+
+class GenerationAdmin(admin.ModelAdmin):
+    inlines = (ModelImageInline, )
+    list_display = ('__str__',)
+
+
+admin.site.register(Brend, BrendAdmin)
+admin.site.register(Model)
+admin.site.register(Category)
+admin.site.register(Equipment, EquipmentAdmin)
+admin.site.register(Generation, GenerationAdmin)
--- avto/models.py
+++ avto/models.py
@@ -12,7 +12,19 @@ class Category(models.Model):
         'self',
         blank=True,
     )
-    slug = models.SlugField()
+    slug = models.SlugField(verbose_name='Слаг')
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
 
 
 class Brend(models.Model):
@@ -21,6 +33,14 @@ class Brend(models.Model):
         unique=True
     )
     country = models.CharField(max_length=50)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Марка'
+        verbose_name_plural = 'Марки'
 
     def __str__(self):
         return self.title
@@ -32,17 +52,33 @@ class Model(models.Model):
         'avto.Category',
         blank=True
     )
+    brends = models.ForeignKey(
+        'avto.Brend',
+        on_delete=models.CASCADE,
+        related_name='models'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Модель'
+        verbose_name_plural = 'Модели'
 
     def __str__(self):
         return self.title
 
 
 class ModelImage(models.Model):
-    description = models.TextField()
-    image = models.ImageField(upload_to='models/%Y/%m/%d/')
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(upload_to='generations/%Y/%m/%d/')
     image_base64 = models.TextField(default=None, null=True, blank=True)
     model = models.ForeignKey(
-        'avto.Model',
+        'avto.Generation',
         related_name='images_model',
         on_delete=models.CASCADE
     )
@@ -61,17 +97,26 @@ class ModelImage(models.Model):
     def image_url(self):
         return self.image_base64 or self.image.url
 
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
+
 
 class Generation(models.Model):
     title = models.CharField(max_length=50)
     year_begin = models.PositiveIntegerField()
     year_end = models.PositiveIntegerField()
-    models = models.ManyToManyField(Model)
+    models = models.ManyToManyField('avto.Model')
+
+
+    class Meta:
+        verbose_name = 'Поколение'
+        verbose_name_plural = 'Поколения'
 
     def __str__(self):
         return self.title
 
-class Characteristic(models.Model):
+class Equipment(models.Model):
 
     class EngineVolume(models.IntegerChoices):
         EIGHT_HUNDRED = 800
@@ -80,7 +125,34 @@ class Characteristic(models.Model):
         ELEVEN_HUNDRED = 1100
         TWELWE_HUNDRED = 1200
         THIRTING_HUNDRED = 1300
-
+        FOURTEEN_HUNDRED = 1400
+        FIFTEEN_HUNDRED = 1500
+        SIXTEEN_HUNDRED = 1600
+        SEVENTEEN_HUNDRED = 1700
+        EIGHTEEN_HUNDRED = 1800
+        NINETEEN_HUNDRED = 1900
+        TWENTY_HUNDRED = 2000
+        TWENTY_ONE_HUNDRED = 2100
+        TWENTY_TWO_HUNDRED = 2200
+        TWENTY_THREE_HUNDRED = 2300
+        TWENTY_FOUR_HUNDRED = 2400
+        TWENTY_FIVE_HUNDRED = 2500
+        TWENTY_SIX_HUNDRED = 2600
+        TWENTY_SEVEN_HUNDRED = 2700
+        TWENTY_EIGHT_HUNDRED = 2800
+        TWENTY_NINE_HUNDRED = 2900
+        THIRTY_HUNDRED = 3000
+        THIRTY_ONE_HUNDRED = 3100
+        THIRTY_TWO_HUNDRED = 3200
+        THIRTY_FIVE_HUNDRED = 3500
+        THIRTY_SIX_HUNDRED = 3600
+        FOURTY_HUNDRED = 4000
+        FOURTY_FIVE_HUNDRED = 4500
+        FIFTY_HUNDRED = 5000
+        FIFTY_FIVE_HUNDRED = 5500
+        SIXTY_HUNDRED = 6000
+        SEVENTY_HUNDRED = 7000
+        EIGHTY_HUNDRED = 8000
 
     class Fuel(models.TextChoices):
         NATURAL_GAS = 'Natural Gas', 'Газ'
@@ -105,6 +177,7 @@ class Characteristic(models.Model):
     class Gearbox(models.TextChoices):
         MANUAL = 'Manual', ' Механическая'
         AUTOMATIC = 'Automatic', 'Автоматическая'
+        ROBOTIC = 'Robotic', 'Робот'
 
     class Drivetrain(models.TextChoices):
         AWD = 'AWD', 'Полный привод'
@@ -132,6 +205,9 @@ class Characteristic(models.Model):
         ELEVEN = 11
         TWELWE = 12
 
+    title = models.CharField(
+        max_length=30,
+    )
     engine_volue = models.IntegerField(
         choices=EngineVolume.choices,
         default=EngineVolume.THIRTING_HUNDRED,
@@ -161,14 +237,12 @@ class Characteristic(models.Model):
         default=Drivetrain.FWD,
         verbose_name='Привод'
     )
-    door = models.CharField(
-        max_length=1,
+    door = models.IntegerField(
         choices=Door.choices,
         default=Door.FOUR,
         verbose_name='Количество дверей'
     )
-    seat = models.CharField(
-        max_length=2,
+    seat = models.IntegerField(
         choices=Seat.choices,
         default=Seat.FIVE,
         verbose_name='Количество мест'
@@ -178,8 +252,36 @@ class Characteristic(models.Model):
         blank=True,
         verbose_name='Пробег'
     )
-    models = models.ForeignKey(
-        'avto.Model',
-        on_delete=models.CASCADE,
-        related_name='Characteristic'
+    consumption_city = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name='Расход город'
     )
+    consumption_road= models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name='Расход трасса'
+    )
+    acceleration = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name='Разгон'
+    )
+    capacity = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True,
+        verbose_name='Мощность'
+    )
+    generations = models.ForeignKey(
+        'avto.Generation',
+        on_delete=models.CASCADE,
+        related_name='Characteristic',
+        verbose_name='Поколение'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Комплектация'
+        verbose_name_plural = 'Комплектации'
